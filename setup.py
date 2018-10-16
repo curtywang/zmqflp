a1e4fc9377a526f3ca8e14b1afcac795e2f0b10a
curtywang <ycwang@u.northwestern.edu>
2018-10-16 11:48:31 -0500

    updated the readme and use cases

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setuptools.setup(
     install_requires=['pyzmq', 'u-msgpack-python'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Mozilla Public License 2.0",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Networking"
     ],
 )
